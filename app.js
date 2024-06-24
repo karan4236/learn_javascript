88a2a77df85b3f22b39ae6cc533fe4f31d636f76
karan4236 <karangautam4236@gmail.com>
2024-06-24 20:45:37 +0530

    new session start

---
+++ app.js
@@ -0,0 +1,175 @@
+
+
+// var a = 100
+// var b = 10
+
+// if(a > b){
+//     document.write("a is greater");
+// }
+
+// var age = 20
+
+// if(age >= 15 && age <= 21){
+//     document.write("what is age");
+//     // console.log("test");
+// }
+
+
+
+
+// .first-excersize
+
+    // var a = 50
+
+    // if(a >=10){
+    //     document.write("this is wheather is positive");
+    // }else {
+    //     document.write("this is weather is negative");
+    // }
+
+
+
+// .second-exersize  2nd pending
+
+// var time =24;
+
+// if(time > 1 && time < 11){
+// document.write("good morning");
+// }else if(time > 11 && time < 15){
+//     document.write("good afternon");
+//     }else if(time > 15 && time < 21){
+//         document.write("good night");
+//         }else(
+//             document.write("valid time.")
+//         )
+
+
+
+
+
+// third...
+
+// var a = 100;
+// var b = 20;
+
+// if(a > b){
+//     document.write("a is greter");
+// } else {
+//     document.write("not greater");
+// }
+
+
+// // turnary operator
+
+// var age = 18;
+// var  max;
+// if(age>=18){
+//     max='yes driver';
+// }else{
+//     max='not driver';
+// };
+// document.write(max);
+
+
+// var age = 18;
+// var max =age>=18 ? 'no drive' : 'yes drive'
+// document.write(max)
+
+
+
+// var age =10;
+// switch(true){
+//     case(age>=18 && age<=40):document.write("you are eligible");
+//     break;
+    // case(2):document.write("fabrary");
+    // break;
+    // case(3):document.write("march");
+    // break;
+    // case(4):document.write("april");
+    // break;
+    // case(5):document.write("may");
+    // break;
+    // case(6):document.write("june");
+    // break;
+    // case(7):document.write("july");
+    // break;
+    // case(8):document.write("august");
+    // break;
+    // case(9):document.write("september");
+    // break;
+    // case(10):document.write("octuber");
+    // break;
+    // case(11):document.write("november");
+    // break;
+    // case(12):document.write("december");
+    // break;
+
+//     default:document.write('please inter valid age')
+// }
+
+
+// alert('this is alertbox')
+
+// var a = 20;
+// var b = 30;
+
+// if(a > b){
+//     alert('a is greter than b')
+// }else{alert('b is greter than a')
+
+// }
+
+
+// var a = confirm('do you likr this website');
+// if(a){
+//     alert('thanks to wesite my site');
+// }else{
+//     alert('its ok');
+// }
+
+
+// var a = prompt('what is your name');
+// document.write('welcome= ' + a);
+
+// var a = prompt('please enter month number');
+// switch(true){
+
+//     case(a==1):document.write('january')
+//     break;
+//     case(a==2):document.write('fabuary')
+//     break;
+//     case(a==3):document.write('march')
+//     break;
+//     case(a==4):document.write('april')
+//     break;
+//     case(a==5):document.write('may')
+//     break;
+//     case(a==6):document.write('june')
+//     break;
+//     case(a==7):document.write('july')
+//     break;
+//     case(a==8):document.write('august')
+//     break;
+//     case(a==9):document.write('september')
+//     break;
+//     case(a==10):document.write('october')
+//     break;
+//     case(a==11):document.write('november')
+//     break;
+//     case(a==12):document.write('december')
+//     break;
+
+//     default:document.write('valid month number')
+// }
+
+function love{
+    let txt1= document.getElementById(txt1).value;
+    let txt2= document.getElementById(txt2).value;
+    if(txt1 && txt2){
+
+        // let total=math.floor(math.random()*100);
+
+    }else{
+        alert("please enter valid name");
+    }
+}
