0d262842a7e60523c7eb6fafd873889cbc3245d3
karan4236 <karangautam4236@gmail.com>
2024-06-27 15:13:07 +0530

    task complate

--- app.js
+++ app.js
@@ -10,4 +10,33 @@ function plus() {
 
 function minus() {
   console.log("----minus work---");
+  let min1 = document.getElementById("value1").value;
+  let min2 = document.getElementById("value2").value;
+  let min3 = parseInt(min1) - parseInt(min2);
+  document.getElementById("result").innerHTML = min3;
+
+  console.log("result", min3);
+}
+
+
+
+function multiply() {
+  console.log("----miltiply work---");
+  let multi1 = document.getElementById("value1").value;
+  let multi2 = document.getElementById("value2").value;
+  let multi3 = parseInt(multi1) * parseInt(multi2);
+  document.getElementById("result").innerHTML = multi3;
+
+  console.log("result", multi3);
+}
+
+
+function multiply() {
+  console.log("----divid work---");
+  let divid1 = document.getElementById("value1").value;
+  let divid2 = document.getElementById("value2").value;
+  let divid3 = parseInt(divid1) / parseInt(divid2);
+  document.getElementById("result").innerHTML = divid3;
+
+  console.log("result", divid3);
 }
